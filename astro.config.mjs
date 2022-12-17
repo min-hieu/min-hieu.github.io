6d92a920772c547ddeb7dec03d7b54c56bfbc08d
민Hieu <53557912+min-hieu@users.noreply.github.com>
2022-12-17 16:34:49 +0900

    updated build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import image from '@astrojs/image';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
-export default defineConfig({
-  site: 'https://min-hieu.github.io',
-  base: '/',
-})
-
 export default {
+  site: 'https://min-hieu.github.io',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
