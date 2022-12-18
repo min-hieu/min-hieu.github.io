7fcd19a4e508d3281bab5fa3129398499a9a11c6
민Hieu <53557912+min-hieu@users.noreply.github.com>
2022-12-18 15:02:37 +0900

    added cv

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-import resume from '../../public/resume.pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+import { RiArrowLeftFill } from '@react-icons/all-files/ri/RiArrowLeftFill';
+import { RiArrowRightFill } from '@react-icons/all-files/ri/RiArrowRightFill';
+import useWindowSize from '../components/useWindowSize'
 
-function CVMain() {
+import '../styles/cv.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+const CVMain = () => {
+  const windowSize = useWindowSize()
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNum, setPageNum] = useState(0);
+  const pdfPath = '/resume.pdf';
+  const pdfWidth = windowSize.width > windowSize.height 
+    ? windowSize.height 
+    : windowSize.width * 0.9
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -12,12 +23,48 @@ function CVMain() {
 
   return (
     <div>
-      <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        {pageNumber} / {numPages}
-      </p>
+      <div className="downCon">
+        <a className="downBtn" href={pdfPath}>
+          download
+        </a>
+      </div>
+      <div className="pdfNumCon">
+        <div 
+          className="btn" 
+          onClick={()=>
+            {pageNum == 0 
+            ? setPageNum(numPages-1) 
+            : setPageNum(pageNum-1)}
+          }
+        >
+          <RiArrowLeftFill />
+        </div>
+        <div className="pdfNum">
+          {pageNum+1}/<b>{numPages}</b>
+        </div>
+        <div 
+          className="btn"
+          onClick={()=>
+            setPageNum((pageNum+1)%numPages)
+          }
+        >
+          <RiArrowRightFill />
+        </div>
+      </div>
+      <div className="pdfCon">
+        <Document 
+          file={pdfPath}
+          onLoadSuccess={onDocumentLoadSuccess}
+          loading="Loading CV..."
+        >
+          <Page 
+            pageNumber={pageNum+1}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            width={pdfWidth}
+          />
+        </Document>
+      </div>
     </div>
   );
 }
