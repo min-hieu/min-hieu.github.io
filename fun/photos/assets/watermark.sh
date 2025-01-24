343b0a60bd67201bbf0946e498e8590b1557352d
민Hieu <53557912+min-hieu@users.noreply.github.com>
2025-01-24 22:56:59 +0900

    replaced font

--- fun/photos/assets/watermark.sh
+++
@@ -1,5 +0,0 @@
-ls | grep .jpg | while read -r filepath; do
-  fname=$filepath:t:r;
-  echo watermarking $fname;
-  magick $fname.jpg \( copyright.png -resize x%\[fx:u.h/0.3\] -alpha set -channel Alpha -evaluate set 50% \) -gravity SouthEast -geometry +15+15 -composite -resize x%\[fx:u.h/5] $fname.webp
-done
