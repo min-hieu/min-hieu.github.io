750f8497973386f523cdddb44f51a3e049bab4e9
민Hieu <53557912+min-hieu@users.noreply.github.com>
2025-03-23 02:08:53 +0900

    added images

---
+++ fun/photos/generate.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup as bs
+from pathlib import Path
+
+
+def getAllImagePath():
+    all_images = list(Path("./assets").glob("*.webp"))
+    exclude_list = ["copyright.webp"]
+    return [img for img in all_images if img.name not in exclude_list]
+
+def readSoup():
+    with open("./template.html") as fp:
+        soup = bs(fp, 'html.parser')
+    return soup
+
+def writeSoup(soup):
+    with open("./index.html", "w") as fp:
+        fp.write(soup.prettify())
+
+def attachImg(container, img_pth):
+    newImg = soup.new_tag('img', src=str(img_pth), alt=img_pth.stem)
+    newImg['class'] = 'photo'
+    container.append(newImg)
+
+soup = readSoup()
+mainCon = soup.find('div', {'id': 'photoMain'})
+
+img_path_list = getAllImagePath()
+for i, img_pth in enumerate(img_path_list):
+    attachImg(mainCon, img_pth)
+
+# print(newImg)
+writeSoup(soup)
+
---
+++ fun/photos/sort.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import imageio.v3 as iio
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import lpips
+import torch
+
+import matplotlib.pyplot as plt
+
+sim_fn = lpips.LPIPS(net='alex')
+all_images = list(Path("./assets").glob("*.webp"))
+exclude_list = ["copyright.webp"]
+all_images = [str(img.resolve()) for img in all_images if img.name not in exclude_list]
+
+sim_mat = torch.zeros(len(all_images), len(all_images))
+with torch.no_grad():
+    for i, img0 in enumerate(tqdm(all_images)):
+        for j, img1 in enumerate(tqdm(all_images, leave=True)):
+            im0tensor = torch.from_numpy(iio.imread(img0)).moveaxis(-1, 0)[None]
+            im1tensor = torch.from_numpy(iio.imread(img1)).moveaxis(-1, 0)[None]
+            im0tensor = torch.nn.functional.interpolate(im0tensor, size=(256, 256))
+            im1tensor = torch.nn.functional.interpolate(im1tensor, size=(256, 256))
+            sim = sim_fn.forward(im0tensor, im1tensor)
+            sim_mat[i, j] = sim
+
+torch.save(sim_mat, "sim_mat.pt")
+import pdb; pdb.set_trace()
